84df3c3ec9964aed1478471ee90a17e0315ea2f0
saurabh13209 <agrawal99saurabh@gmail.com>
2019-02-16 21:02:29 +0530

    000-default.conf change automated

---
+++ Bash File/setDir.sh
@@ -0,0 +1,37 @@
+echo ""
+echo "$1"
+echo ""
+sudo echo '<VirtualHost *:80>' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# The ServerName directive sets the request scheme, hostname and port that' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# the server uses to identify itself. This is used when creating' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# redirection URLs. In the context of virtual hosts, the ServerName' >> /etc/apache2/sites-available/000-default.conf
+sudo echo "	# specifies what hostname must appear in the request's Host: header to" >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# match this virtual host. For the default virtual host (this file) this' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# value is not decisive as it is used as a last resort host regardless.' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# However, you must set it for any further virtual host explicitly.' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	#ServerName www.example.com' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	ServerAdmin webmaster@localhost' >> /etc/apache2/sites-available/000-default.conf
+
+sudo echo "	DocumentRoot ${1}" >> /etc/apache2/sites-available/000-default.conf
+sudo echo '' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# Available loglevels: trace8, ..., trace1, debug, info, notice, warn,' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# error, crit, alert, emerg.' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# It is also possible to configure the loglevel for particular' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# modules, e.g.' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	#LogLevel info ssl:warn' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	ErrorLog ${APACHE_LOG_DIR}/error.log' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	CustomLog ${APACHE_LOG_DIR}/access.log combined' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# For most configuration files from conf-available/, which are' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# enabled or disabled at a global level, it is possible to' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# include a line for only one particular virtual host. For example the' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# following line enables the CGI configuration for this host only' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	# after it has been globally disabled with "a2disconf".' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '	#Include conf-available/serve-cgi-bin.conf' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '</VirtualHost>' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '' >> /etc/apache2/sites-available/000-default.conf
+sudo echo '# vim: syntax=apache ts=4 sw=4 sts=4 sr noet' >> /etc/apache2/sites-available/000-default.conf
+
+#cp index.php $1
--- Bash File/setupHost.sh
+++ Bash File/setupHost.sh
@@ -1,48 +1,60 @@
-echo "Welcome to SFR setup."
+echo "Welcome to Server File Reader setup."
 echo "Continue installation?(Y/N)"
 
 read  contInst
 if(let "contInst = 89");  then
     sudo apt-get update
-fi
-echo ""
-echo "******************** Files updated ********************"
-echo ""
-
-echo "Install Apache? (Y/N)"
-read installApache
-if(let "installApache = 89");  then
+    echo ""
+    echo "******************** Files updated ********************"
+    echo ""
     sudo apt-get install apache2
-fi
-echo ""
-echo "******************** Apache updated ********************"
-echo ""
-
-i=1
-for d in /home/* ; do
-    echo "${i} ${d}"
-    let "i++"
-done
-echo "choose your parent directory"
-read parentDir
-if (let "parentDir = 1"); then
-    sudo gedit /etc/apache2/sites-available/000-default.conf
-    sudo service apache2 restart
-
+    echo ""
+    echo "******************** Apache updated ********************"
+    echo ""
+    clear
+    echo "Task completed:"
+    echo "1. Files updated"
+    echo "2. Apache installed"
+    echo ""
+    echo "choose your parent directory"
+    echo "*ch	parent_directory "
+    i=1
+    for d in /home/* ; do
+        echo "${i} ${d}"
+        let "i++"
+    done
+    echo "select any one"
+    read parentDir
+    i=1
+    for d in /home/* ; do
+        if(let "i = ${parentDir}"); then
+            sudo sh ./setDir.sh $d
+            break
+        fi
+    done
     sudo rm -rf /etc/apache2/apache2.conf
-
     sudo sh ./master.sh
-    sudo service apache2 restart
 
     sudo ufw app info "Apache Full"
     sudo apt-get install mysql-server
+    echo ""
+    echo "******************** MySql installed successfully ********************"
+    echo ""
     sudo apt-get install php libapache2-mod-php php-mcrypt php-mysql
-    sudo systemctl restart apache2
-    
-    
 
-else
-    echo "NN"
+    sudo service apache2 restart
+    sudo systemctl restart apache2
+
+    echo ""
+    echo "******************** Setup installed successfully ********************"
+    echo ""
+    echo "Your server ip is :"
+    sudo ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'
+    
 fi
+
+
+
+
  
 
